6ba54ed4e2a546203222a6240e95cc518f1b9d8e
benjaminreis <benjaminreis@gmail.com>
2018-11-07 06:38:46 -0600

    factory

---
+++ TodoAPI/TodoAPI/Factory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace TodoAPI
+{
+    internal class Factory
+    {
+        internal Factory()
+        {
+        }
+
+
+
+        private Managers.TodoManager _TodoManager;
+
+    internal Managers.TodoManager TodoManager
+        {
+            get {
+                if (_TodoManager == null)
+                {
+                    _TodoManager = new Managers.TodoManager();
+                }
+                return _TodoManager;
+            }
+            set
+            {
+                _TodoManager = value;
+            }
+                
+        }
+
+
+
+
+
+    }
+}
